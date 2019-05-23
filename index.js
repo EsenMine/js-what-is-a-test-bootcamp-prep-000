6a46989ebc79b60bc19cc4bea66bdc97a3688b82
Miné E <mine_esen@yahoo.ca>
2019-05-23 18:53:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // write your code below!
-var firstName = "Mine"
-var height = "74"
-var message = "${firstName} is ${height} inches tall"
+var firstName = "Mine";
+var height = "74";
+var message = "${firstName} is ${height} inches tall";
